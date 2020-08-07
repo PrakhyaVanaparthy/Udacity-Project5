9cfa1991cf920c472242c9e0294e60f2dffc1bfc
Prakhya Vanaparthy <prakhya_vanaparthy@yahoo.com>
2020-08-07 14:47:54 -0400

    added output prediction logging

--- app.py
+++ app.py
@@ -61,8 +61,9 @@ def predict():
     # scale the input
     scaled_payload = scale(inference_payload)
     # get an output prediction from the pretrained model, clf
-    prediction = list(clf.predict(scaled_payload))
+    prediction = list(clf.predict(scaled_payload)
     # TO DO:  Log the output prediction value
+    LOG.info(f"output prediction: {prediction}")
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
