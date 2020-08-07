f0ac3246aef30c83acd7a558aedcf451afffbb67
Prakhya Vanaparthy <prakhya_vanaparthy@yahoo.com>
2020-08-07 14:46:08 -0400

    updated the bash file to upload docker image to docker hub

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,11 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-dockerpath=PrakhyaVanaparthy/project/project5
+dockerpath=prakhyavanaparthy/project5:project5
 
 # Step 2:  
 # Authenticate & tag
-echo "8e42ff38a8e7 and project5: $dockerpath"
+echo "06bc36bf258b and project5: $dockerpath"
+docker login
+docker tag project5 $dockerpath
 
 # Step 3:
 # Push image to a docker repository
